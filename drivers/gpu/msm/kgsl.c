b9e372f2dfa529fbe00b1e5ae03f78cdd17b22e1
klozz <shaclone707@gmail.com>
2019-01-01 23:28:17 -0500

    kgsl:use __get_user_pages get_user_pages was remplaced in this commit 40f62dd
    Signed-off-by: klozz <shaclone707@gmail.com>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -2109,7 +2109,7 @@ static int memdesc_sg_virt(struct kgsl_memdesc *memdesc, struct file *vmfile)
 				vmfile, memdesc);
 
 	if (ret == 0) {
-		npages = get_user_pages(current, current->mm, memdesc->useraddr,
+		npages = __get_user_pages(current, current->mm, memdesc->useraddr,
 					sglen, write, 0, pages, NULL);
 		ret = (npages < 0) ? (int)npages : 0;
 	}
