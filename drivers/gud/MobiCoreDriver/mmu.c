73600520d01d1390c942d1f62cd4206f37a23857
klozz <shaclone707@gmail.com>
2019-01-01 23:35:47 -0500

    MobiCoreDriver:use __get_user_pages get_user_pages was remplaced in this commit 40f62dd
    Signed-off-by: klozz <shaclone707@gmail.com>

--- drivers/gud/MobiCoreDriver/mmu.c
+++ drivers/gud/MobiCoreDriver/mmu.c
@@ -347,11 +347,11 @@ struct tee_mmu *tee_mmu_create(struct mm_struct *mm,
 
 			/* Buffer was allocated in user space */
 			down_read(&mm->mmap_sem);
-			gup_ret = get_user_pages(NULL, mm, (uintptr_t)reader,
+			gup_ret = __get_user_pages(NULL, mm, (uintptr_t)reader,
 						 pages_nr, 1, 0, pages,
 						 NULL);
 			if ((gup_ret == -EFAULT) && !write) {
-				gup_ret = get_user_pages(
+				gup_ret = __get_user_pages(
 						NULL, mm, (uintptr_t)reader,
 						pages_nr, 0, 0, pages,
 						NULL);
